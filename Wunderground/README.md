b84f3920188e6a79ad47f09ee7e6cf0e42012993
Slobodan Stipic <slobo80@gmail.com>
2015-02-22 14:53:54 -0800

    Added wunderground

---
+++ Wunderground/README.md
@@ -0,0 +1,3 @@
+﻿# Wunderground
+
+
---
+++ Wunderground/app.js
@@ -0,0 +1,15 @@
+﻿var myKey = '67ef6acb8895b29b';
+var Temp = require('./Temp.js');
+var Wunderground = require('wunderground-api');
+
+Temp.init(function (err, data) {
+    if (err) {
+        throw err
+    }            
+    else {
+        console.log(data.temp_f);
+    }
+});
+
+Temp.current("Seattle", "WA");
+
