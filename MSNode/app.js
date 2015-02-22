a95c9b2626f1ffdc3665809d2b4d2ff37ae28d9f
Slobodan Stipic <slobo80@gmail.com>
2015-02-22 14:47:25 -0800

    Factored out api calls into main.js

--- MSNode/app.js
+++ MSNode/app.js
@@ -1,41 +1,44 @@
-﻿
-var fs = require('fs');
-var AzureMedia = require('azure-media');
-var myconfig = require('./myconfig');
+﻿var Main = require('./main.js');
 
-var api = new AzureMedia(myconfig);
-api.init(function (err) {
+Main.init();
+//var api = new AzureMedia(myconfig);
+//api.init(function (err) {
     
-    api.rest.asset.list(function (err, results) {
-        results.forEach(function (result) {
-            api.rest.asset.get(result.Id, function (err, asset) {
-                var path = 'audio/' + asset.Name;
-                console.log(path);
-                api.media.downloadStream(asset.Id, fs.createWriteStream(path, function (err) {
+//    api.rest.asset.list(function (err, results) {
+//        console.log(err);
+
+//        results.forEach(function (result) {
+//            api.rest.asset.get(result.Id, function (err, asset) {
+//                console.log(err);
+//                var path = 'audio/' + asset.Name;
+//                console.log(path);
+//                api.media.downloadStream(asset.Id, fs.createWriteStream(path, function (err) {
                     
-                   // console.log(err);
-                }));
+//                    console.log(err);
+//                }));
 
 
-            });
-            //api.rest.asset.Files(result.Id, function (errrawd) {
-            //    console.log(rawd.Name);
-            //});
+//            });
+//            //api.rest.asset.Files(result.Id, function (errrawd) {
+//            //    console.log(rawd.Name);
+//            //});
 
-            //api.media.getDownloadURL(result.Id, function (err, url) {
-            //    console.log(err);
-            //});
+//            //api.media.getDownloadURL(result.Id, function (err, url) {
+//            //    console.log(err);
+//            //});
 
-                //az.rest.asset.delete(obj.Id, function (err) {
-                //    idx += 1;
-                //    if (idx === length) {
-                //        test.done();
-                //    }
-                //});
-            //});
-        });
-    },
-    { '$filter': "endswith(Name, '.mp3')" });
-});
+//                //az.rest.asset.delete(obj.Id, function (err) {
+//                //    idx += 1;
+//                //    if (idx === length) {
+//                //        test.done();
+//                //    }
+//                //});
+//            //});
+//        });
+//    },
+//    { '$filter': "endswith(Name, '.mp3')" });
+
+//    console.log(err);
+//});
 
 //console.log('Done');
